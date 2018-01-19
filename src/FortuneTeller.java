c87cc3e2b422c7bdeec09124514917cec759ce30
cooksey3 <chris@cookseys.org>
2018-01-19 10:25:10 -0500

    Step 1 - Ask for user input

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
 
 public class FortuneTeller {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner (System.in);
+		
+//		System.out.println("Please write your first name: ");
+//		String firstName = input.nextLine(); 
+//		
+//		System.out.println("Please write your last name: ");
+//		String lastName = input.nextLine();
+//		
+//		System.out.println("Please write your birth month: ");
+//		String birthMonth = input.nextLine();
+//		
+		
+		System.out.print("Please write your age: ");
+		int age = input.nextInt(); 
+
+		System.out.print("Please write your number of siblings: ");
+		int numberOfSiblings = input.nextInt(); 
+
+		System.out.println("Please write your favorite ROYGBIV color or \"help\": ");
+		String favoriteColor = input.nextLine();
 
 	}
 
